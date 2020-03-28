2cc0e664ea160dcd1210f939fe090cf9be088c13
Patrick Goelre <patrickvongoeler@gmail.com>
2020-03-28 20:39:06 +0100

    add secret key to access firestore locally

--- backend/functions/src/utils/admin.ts
+++ backend/functions/src/utils/admin.ts
@@ -1,8 +1,11 @@
 import * as admin from "firebase-admin";
-// import { firebaseConfig } from "./config";
+import { adminConfig } from "./secret";
 
-admin.initializeApp();
+admin.initializeApp({
+    credential: admin.credential.cert(adminConfig),
+    databaseURL: "https://shourl-62076.firebaseio.com"
+});
 
 const db = admin.firestore();
 
-export {admin, db};
+export { admin, db };
