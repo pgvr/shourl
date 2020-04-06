be6ca7409886e6c1d5322c402bf854b4176c1d05
Patrick Goeler <patrickvongoeler@gmail.com>
2020-04-06 16:06:33 +0200

    frontend styling

--- frontend/src/routes/_layout.svelte
+++ frontend/src/routes/_layout.svelte
@@ -5,7 +5,50 @@
 
 <style lang="postcss">
     main {
-        @apply relative max-w-4xl bg-white p-8 mx-auto my-0;
+        @apply relative bg-gray-100 h-full w-full mx-auto my-0;
+
+        /* background-color: #21d4fd;
+        background-image: linear-gradient(19deg, #21d4fd 0%, #b721ff 100%); */
+        background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+        background-size: 200% 200%;
+        -webkit-animation: Gradient 15s ease infinite;
+        -moz-animation: Gradient 15s ease infinite;
+        animation: Gradient 15s ease infinite;
+    }
+    @-webkit-keyframes Gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+
+    @-moz-keyframes Gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+
+    @keyframes Gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
     }
 </style>
 
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -2,41 +2,95 @@
     let link = ""
     let result = null
     import { linkLoading } from "../stores/ui.store"
-    function submit(event) {
-        console.log(link)
-        linkLoading.update(l => !l)
-        setTimeout(() => {
+    import Loader from "../components/Loading.svelte"
+    async function submit(event) {
+        if (!link) {
+            console.log("link empty")
+            return
+        }
+        try {
+            console.log(link)
             linkLoading.update(l => !l)
-            result = true
-        }, 2000)
+            const res = await fetch("http://localhost:4000/encode", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify({ longUrl: link }),
+            })
+            const body = await res.json()
+            result = decodeURI(body.data.encodedEmojis)
+            link = "https://ourdomain.com/" + result
+            copyToClipboard(link)
+        } catch (error) {
+            console.log(error)
+        }
+        linkLoading.update(l => !l)
+    }
+
+    function copyToClipboard(value) {
+        const el = document.createElement("textarea")
+        el.value = value
+        el.setAttribute("readonly", "")
+        el.style.position = "absolute"
+        el.style.left = "-9999px"
+        document.body.appendChild(el)
+        el.select()
+        document.execCommand("copy")
+        document.body.removeChild(el)
     }
 </script>
 
-<svelte:head>
-    <title>Shourl</title>
-</svelte:head>
+<style>
+    header {
+        @apply flex items-center justify-center;
+        height: 40vh;
+    }
+</style>
 
-<h1 class="text-red-500 text-6xl text-center">Shourl</h1>
-<form class="mt-4" on:submit|preventDefault={submit}>
-    <div class="flex flex-wrap flex-col md:flex-row">
-        <!-- <label class="block text-gray-700 text-sm font-bold mb-2" for="link">Username</label> -->
-        <input
-            readonly={result}
-            class:border-green-500={result}
-            class="w-full md:w-auto md:flex-grow shadow appearance-none border rounded py-2 px-3 text-gray-700
-            leading-tight focus:outline-none focus:shadow-outline"
-            bind:value={link}
-            id="link"
-            type="text"
-            placeholder="heckinglongurl.com" />
-        <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none
-            focus:shadow-outline md:ml-4 w-full md:w-auto mt-4 md:mt-0 md:flex-shrink-0"
-            type="submit">
-            Shorten
-        </button>
+<svelte:head>
+    <title>🖇️</title>
+</svelte:head>
+<header class="header">
+    <div class="py-4 px-8 rounded bg-gray-900 text-white">
+
+        <h1 class="text-6xl">🖇️</h1>
+        <p class="text-center text-gray-400">😄, 👨🏻‍💻, 💾</p>
     </div>
+</header>
+
+<form class="w-full max-w-xl mx-auto px-4 relative" on:submit|preventDefault={submit}>
     {#if $linkLoading}
-        <div>Loading...</div>
+        <div class=" absolute left-0 right-0" style="top: 8px;">
+            <Loader />
+        </div>
+    {/if}
+    <input
+        readonly={result}
+        class:border-green-500={result}
+        class:opacity-50={$linkLoading}
+        class="w-full rounded border shadow h-24 appearance-none py-2 px-3 text-gray-700 leading-tight
+        focus:outline-none focus:shadow-outline"
+        bind:value={link}
+        id="link"
+        type="text"
+        placeholder="heckinglongurl.com" />
+
+    {#if result}
+        <button
+            class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded focus:outline-none
+            focus:shadow-outline w-full mt-4"
+            type="button"
+            on:click={copyToClipboard(link)}>
+            ➡️ 📋
+        </button>
+    {:else}
+        <button
+            class="bg-purple-700 hover:bg-purple-900 h-12 text-white font-bold py-2 px-4 rounded focus:outline-none
+            focus:shadow-outline w-full mt-4 "
+            type="submit">
+            ➡️⬅️
+        </button>
     {/if}
 </form>
